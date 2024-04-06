dd094c38febad6e544f14326e44d5924bc720ee3
Bintang <itzbintangyosua@gmail.com>
2024-04-06 20:31:13 +0700

    blogs to blog on navbar

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -5,7 +5,7 @@
 <nav class="sticky top-0 z-10 bg-white">
   <div class="container py-4">
     <div
-      class="flex md:flex-row justify-between px-4 md:px-8 lg:px-32 xl:px-64 items-center flex-col gap-3"
+      class="flex flex-col items-center justify-between gap-3 px-4 md:flex-row md:px-8 lg:px-32 xl:px-64"
     >
       <span class="text-3xl font-bold" id="title"
         ><a href="/">Minuettaro</a></span
@@ -14,7 +14,7 @@
         <!-- Nav Items -->
         <a href="/contact">Contact</a>
         <span><a href="/projects">Projects</a></span>
-        <span><a href="/blog">Blogs</a></span>
+        <span><a href="/blog">Blog</a></span>
       </div>
     </div>
   </div>
